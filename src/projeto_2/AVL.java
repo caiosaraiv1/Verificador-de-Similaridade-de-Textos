79fc83ecaf4afe3c7114a850641bddf2e52a75bf
caiosaraiv1 <caio.ariel@outlook.com>
2025-11-06 18:13:44 -0300

    Atualizei a classe nó e fiz o algoritmo de inserção na avl

--- src/projeto_2/AVL.java
+++ src/projeto_2/AVL.java
@@ -18,8 +18,8 @@ package projeto_2;
  */
 public class AVL {
 
-	No raiz;
-	int contadorRotacoes;
+	private No raiz;
+	private int contadorRotacoes;
 	
 	public AVL() {
 		this.raiz = null;
@@ -31,4 +31,93 @@ public class AVL {
 		contadorRotacoes = 0;
 	}
 	
+	private int altura(No n) {
+		if (n == null) return 0;
+		return n.getAltura();
+	}
+	
+	private int fatorBalanceamento(No n) {
+		if (n == null) return 0;
+		return altura(n.getEsq()) - altura(n.getDir());
+	}
+	
+	public void inserir(Resultado res) {
+		this.raiz = inserirRecursivo(this.raiz , res);
+	}
+	
+	private No inserirRecursivo(No noAtual, Resultado novoRes) {
+		if (noAtual == null) {
+			No novoNo = new No(novoRes);
+			return novoNo;
+		} else if (novoRes.getSimilaridade() == noAtual.getSimilaridade()) {
+			noAtual.adicionarResultado(novoRes);
+			return noAtual;
+		} else {
+			if (novoRes.getSimilaridade() < noAtual.getSimilaridade()) {
+			    noAtual.setEsq(inserirRecursivo(noAtual.getEsq(), novoRes)); 
+			} else { 
+			    noAtual.setDir(inserirRecursivo(noAtual.getDir(), novoRes)); 
+			}
+		}
+		
+		noAtual.setAltura(1 + Math.max(altura(noAtual.getEsq()), altura(noAtual.getDir())));
+		
+		int fb = fatorBalanceamento(noAtual);
+		
+		if (fb > 1 && novoRes.getSimilaridade() < noAtual.getEsq().getSimilaridade()) {
+			return rotacaoDireita(noAtual);
+		}
+		
+		if (fb < -1 && novoRes.getSimilaridade() > noAtual.getDir().getSimilaridade()) {
+			return rotacaoEsquerda(noAtual);
+		}
+		
+		if (fb > 1 && novoRes.getSimilaridade() > noAtual.getEsq().getSimilaridade()) {
+			noAtual.setEsq(rotacaoEsquerda(noAtual.getEsq()));
+			return rotacaoDireita(noAtual);
+		}
+		
+		if (fb < -1 && novoRes.getSimilaridade() < noAtual.getDir().getSimilaridade()) {
+			noAtual.setDir(rotacaoDireita(noAtual.getDir()));
+	        return rotacaoEsquerda(noAtual);
+		}
+		
+		return noAtual;
+	}
+	
+	private No rotacaoDireita(No y) {
+	    No x = y.getEsq();      
+	    No T2 = x.getDir();     
+
+	    x.setDir(y);            
+	    y.setEsq(T2);           
+
+	    y.setAltura(1 + Math.max(altura(y.getEsq()), altura(y.getDir())));
+	    x.setAltura(1 + Math.max(altura(x.getEsq()), altura(x.getDir())));
+
+	    this.contadorRotacoes += 1;
+
+	    return x;
+	}
+	
+	private No rotacaoEsquerda(No x) { 
+	    No y = x.getDir();     
+	    No T2 = y.getEsq();     
+
+	    y.setEsq(x);            
+	    x.setDir(T2);           
+
+	    x.setAltura(1 + Math.max(altura(x.getEsq()), altura(x.getDir())));
+	    y.setAltura(1 + Math.max(altura(y.getEsq()), altura(y.getDir())));
+
+	    this.contadorRotacoes += 1;
+
+	    return y; 
+	}
+	
+	/*
+	 * TODO: método de busca por lista -> exibe todos os pares com similaridade acima do limiar;
+	 * TODO: método de busca topK -> exibe apenas os K pares mais semelhantes (K informado);
+	 * TODO: método de busca -> compara dois arquivos específicos (o limiar é ignorado neste modo);
+	 */
 }
--- src/projeto_2/No.java
+++ src/projeto_2/No.java
@@ -5,26 +5,61 @@ import java.util.List;
 
 public class No {
 
-	double similaridade;
-	List<Resultado> resultados;
-	No esq;
-	No dir;
-	int altura;
-	
-	public No(Resultado resultado) {
-		this.similaridade = resultado.getSimilaridade();
-		this.resultados = new ArrayList<>();
-		this.resultados.add(resultado);
-		this.esq = null;
-		this.dir = null;
-		this.altura = 0;
-	}
-	
-	@Override
-	public String toString() {
-		return "No[Similaridade]: " + this.similaridade +
-				", Altura: " + this.altura + 
-				", Qtd. Resultados: " + (this.resultados != null ? this.resultados.size() : 0) + 
-				"]";
-	}
+    private double similaridade;
+    private List<Resultado> resultados;
+    private No esq;
+    private No dir;
+    private int altura;
+    
+    public No(Resultado resultado) {
+        this.similaridade = resultado.getSimilaridade();
+        this.resultados = new ArrayList<>();
+        this.resultados.add(resultado);
+        this.esq = null;
+        this.dir = null;
+        this.altura = 1;
+    }
+
+    public double getSimilaridade() {
+        return similaridade;
+    }
+
+    public List<Resultado> getResultados() {
+        return resultados;
+    }
+
+    public void adicionarResultado(Resultado resultado) {
+        this.resultados.add(resultado);
+    }
+
+    public No getEsq() {
+        return esq;
+    }
+
+    public void setEsq(No esq) {
+        this.esq = esq;
+    }
+
+    public No getDir() {
+        return dir;
+    }
+
+    public void setDir(No dir) {
+        this.dir = dir;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    @Override
+    public String toString() {
+        return "No[Similaridade: " + String.format("%.4f", this.similaridade) + 
+               " | Altura: " + this.altura + 
+               " | Qtd. Resultados: " + this.resultados.size() + "]";
+    }
 }
