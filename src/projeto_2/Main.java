4eaa7caa58c91e0513f2b6e9e36e0dcb551de5a3
caiosaraiv1 <caio.ariel@outlook.com>
2025-11-12 14:38:20 -0300

    Revert "Alteração avl"
    This reverts commit 48558edd527bb7daafa3f6dffa4d298ba8727488.

--- src/projeto_2/Main.java
+++ src/projeto_2/Main.java
@@ -1,164 +1,121 @@
 package projeto_2;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class AVL {
 
-	private No raiz;
-	private int contadorRotacoes;
+public class Main {
 	
-	public AVL() {
-		this.raiz = null;
-		contadorRotacoes = 0;
-	}
-	
-	public AVL(No no) {
-		this.raiz = no;
-		contadorRotacoes = 0;
-	}
-	
-	public int getContadorRotacoes() {
-		return contadorRotacoes;
-	}
+	private static int contadorDocumentos = 0;
+	private static int contadorComparacoes = 0;
 
-	private int altura(No n) {
-		if (n == null) return 0;
-		return n.getAltura();
-	}
-	
-	private int fatorBalanceamento(No n) {
-		if (n == null) return 0;
-		return altura(n.getEsq()) - altura(n.getDir());
-	}
-	
-	public void inserir(Resultado res) {
-		this.raiz = inserirRecursivo(this.raiz , res);
-	}
-	
-	private No inserirRecursivo(No noAtual, Resultado novoRes) {
-		if (noAtual == null) {
-			No novoNo = new No(novoRes);
-			return novoNo;
-		} else if (novoRes.getSimilaridade() == noAtual.getSimilaridade()) {
-			noAtual.adicionarResultado(novoRes);
-			return noAtual;
-		} else {
-			if (novoRes.getSimilaridade() < noAtual.getSimilaridade()) {
-			    noAtual.setEsq(inserirRecursivo(noAtual.getEsq(), novoRes)); 
-			} else { 
-			    noAtual.setDir(inserirRecursivo(noAtual.getDir(), novoRes)); 
-			}
-		}
-		
-		noAtual.setAltura(1 + Math.max(altura(noAtual.getEsq()), altura(noAtual.getDir())));
-		
-		int fb = fatorBalanceamento(noAtual);
-		
-		if (fb > 1 && novoRes.getSimilaridade() < noAtual.getEsq().getSimilaridade()) {
-			return rotacaoDireita(noAtual);
-		}
-		
-		if (fb < -1 && novoRes.getSimilaridade() > noAtual.getDir().getSimilaridade()) {
-			return rotacaoEsquerda(noAtual);
-		}
-		
-		if (fb > 1 && novoRes.getSimilaridade() > noAtual.getEsq().getSimilaridade()) {
-			noAtual.setEsq(rotacaoEsquerda(noAtual.getEsq()));
-			return rotacaoDireita(noAtual);
-		}
-		
-		if (fb < -1 && novoRes.getSimilaridade() < noAtual.getDir().getSimilaridade()) {
-			noAtual.setDir(rotacaoDireita(noAtual.getDir()));
-	        return rotacaoEsquerda(noAtual);
-		}
-		
-		return noAtual;
-	}
-	
-	private No rotacaoDireita(No y) {
-	    No x = y.getEsq();      
-	    No T2 = x.getDir();     
+    public static void main(String[] args) throws IOException {
 
-	    x.setDir(y);            
-	    y.setEsq(T2);           
-
-	    y.setAltura(1 + Math.max(altura(y.getEsq()), altura(y.getDir())));
-	    x.setAltura(1 + Math.max(altura(x.getEsq()), altura(x.getDir())));
-
-	    this.contadorRotacoes += 1;
-
-	    return x;
-	}
-	
-	private No rotacaoEsquerda(No x) { 
-	    No y = x.getDir();     
-	    No T2 = y.getEsq();     
-
-	    y.setEsq(x);            
-	    x.setDir(T2);           
-
-	    x.setAltura(1 + Math.max(altura(x.getEsq()), altura(x.getDir())));
-	    y.setAltura(1 + Math.max(altura(y.getEsq()), altura(y.getDir())));
-
-	    this.contadorRotacoes += 1;
-
-	    return y; 
-	}
-	
-	public Resultado getResultadoMenorSimilaridade() {
-        if (this.raiz == null) return null;
-
-        No noAtual = this.raiz;
+    	String caminho = args[0];
+    	double similaridade = Double.parseDouble(args[1]);
+    	Map<String, Documento> tabelaArquivos = Main.getTabelaArquivos(caminho);
+    	AVL arvore = Main.popularAVL(tabelaArquivos);
+    	List<Resultado> lista = new ArrayList<Resultado>();
+    	
+    	if (args.length == 3) {
+    		lista = arvore.lista(similaridade);
+    		Main.printarSaida(similaridade, lista, arvore);
+    	} else if (args.length == 4) {
+    		int K = Integer.parseInt(args[3]);
+    		lista = arvore.topK(K, similaridade);
+    		Main.printarSaida(similaridade, lista, arvore);
+    	} else if (args.length == 5) {
+    		double resultadoBusca = Main.busca(args[3], args[4], tabelaArquivos);
+    		Main.printarSaida(args[3], args[4], resultadoBusca);
+    	}
         
-        while (noAtual.getEsq() != null) noAtual = noAtual.getEsq();
-  
-        return noAtual.getResultados().get(0);
-    }
-	
-	public List<Resultado> lista(double similaridadeMinima) {
-		List<Resultado> resultadosEncontrados = new ArrayList<>();
-		listaRecursivo(this.raiz, similaridadeMinima, resultadosEncontrados);
-		return resultadosEncontrados;
-	}
-	
-	private void listaRecursivo(No noAtual, double similaridadeMinima, List<Resultado> lista) {
-		if (noAtual == null) return;
-		if (noAtual.getSimilaridade() <= similaridadeMinima) {
-			listaRecursivo(noAtual.getDir(), similaridadeMinima, lista);
-		} else if (noAtual.getSimilaridade() > similaridadeMinima) {
-			lista.addAll(noAtual.getResultados());
-			listaRecursivo(noAtual.getEsq(), similaridadeMinima, lista);
-			listaRecursivo(noAtual.getDir(), similaridadeMinima, lista);
-		}
-	}
-	
-	public List<Resultado> topK(int K, double similaridadeMinima) {
-		if (K <= 0) return new ArrayList<>();
-		List<Resultado> resultadosEncontrados = new ArrayList<>();
-		topKRecursivo(this.raiz, K, similaridadeMinima, resultadosEncontrados);
-		return resultadosEncontrados;
-	}
-	
-	private void topKRecursivo(No noAtual, int K, double similaridadeMinima, List<Resultado> lista) {
-		if (noAtual == null) return;
-		if (lista.size() >= K) return;
-		
-		topKRecursivo(noAtual.getDir(), K, similaridadeMinima, lista);
-		
-		if (lista.size() >= K) return; // Se a direita já encheu a lista, pare
-		
-		if (noAtual.getSimilaridade() > similaridadeMinima) {
-			List<Resultado> listaAux = noAtual.getResultados();
-			for (Resultado res : listaAux) {
-				if (lista.size() < K) lista.add(res);
-				else break;
-			}
-			
-			if (lista.size() >= K) return;
-			
-			topKRecursivo(noAtual.getEsq(), K, similaridadeMinima, lista);
-		}
-	}
-}
+        
 
+    }
+    
+    public static double busca(String arquivoA, String arquivoB, Map<String, Documento> tabelaArquivos) {
+    	if (arquivoA.equals(arquivoB)) return 1.0;
+    	
+    	Documento docA = tabelaArquivos.get(arquivoA);
+    	Documento docB = tabelaArquivos.get(arquivoB);
+    	
+    	if (docA == null || docB == null) throw new IllegalArgumentException("Arquivos invalidos.");
+    	
+    	TabelaHash tabA = docA.getTabela();
+    	TabelaHash tabB = docB.getTabela();
+    	
+    	ComparadorCosseno comparador = new ComparadorCosseno(tabA, tabB);
+    	
+    	return comparador.similaridade();
+    }
+    
+    public static Map<String, Documento> getTabelaArquivos(String caminhoPasta) throws IOException {
+    	File pasta = new File(caminhoPasta);
+        if (!pasta.isDirectory()) throw new IllegalStateException("O caminho especificado não é um diretório válido: " + pasta.getPath());
+        
+        File[] arquivosPasta = pasta.listFiles();
+        if (arquivosPasta == null) throw new IOException("Não foi possível listar os arquivos da pasta: " + pasta.getPath());
+        
+        Map<String, Documento> tabelaArquivos = new HashMap<String, Documento>();
+        for (File f: arquivosPasta) {
+        	if (f.isFile()) {
+				tabelaArquivos.put(f.getName(), new Documento(f.getPath()));
+				contadorDocumentos++;
+        	}
+        }
+        return tabelaArquivos;
+    }
+    
+    public static AVL popularAVL(Map<String, Documento> tabelaArquivos) {
+    	List<Documento> lista = new ArrayList<>(tabelaArquivos.values());
+    	AVL arvore = new AVL();
+
+    	for (int i = 0; i < lista.size(); i++) {
+    		for (int j = i + 1; j < lista.size(); j++) {
+    			
+    			Documento docA = lista.get(i);
+    			Documento docB = lista.get(j);
+    			
+    			TabelaHash tabA = docA.getTabela();
+    			TabelaHash tabB = docB.getTabela();
+    			
+    			ComparadorCosseno comparador = new ComparadorCosseno(tabA, tabB);
+    			double similaridade = comparador.similaridade();
+    			
+    			Resultado res = new Resultado(docA.getNomeArquivo(), docB.getNomeArquivo(), similaridade);
+    			arvore.inserir(res);
+    			contadorComparacoes++;
+    		}
+    	}
+    	
+    	return arvore;
+    }
+    
+    public static void printarSaida(double similaridade, List<Resultado> lista, AVL arvore) {
+    	System.out.println("=== VERIFICADOR DE SIMILARIDADE DE TEXTOS ===");
+    	System.out.println("Total de documentos processados: " + contadorDocumentos);
+    	System.out.println("Total de pares comparados: " + contadorComparacoes);
+    	System.out.println("Função hash utilizada:");
+    	System.out.println("Métrica de similaridade: Cosseno");
+    	System.out.println("Pares com similaridade >= " + similaridade + ":");
+    	System.out.println("---------------------------------");
+    	for (Resultado r : lista) {
+    		System.out.println(r.getArquivoA() + " <-> " + r.getArquivoB() + " = " + r.getSimilaridade());
+    	}
+    	System.out.println("Pares com menor similaridade:");
+    	System.out.println("---------------------------------");
+    	Resultado menor = arvore.getResultadoMenorSimilaridade();
+    	System.out.println(menor.getArquivoA() + " <-> " + menor.getArquivoB() + " = " + menor.getSimilaridade());
+    }
+    
+    public static void printarSaida(String arquivoA, String arquivoB, double similaridade) {
+    	System.out.println("=== VERIFICADOR DE SIMILARIDADE DE TEXTOS ===");
+    	System.out.println("Comparada: " + arquivoA + " <-> " + arquivoB);
+    	System.out.println("Similaridade calculada: " + similaridade);
+    	System.out.println("Métrica de similaridade: Cosseno");
+    }
+}
