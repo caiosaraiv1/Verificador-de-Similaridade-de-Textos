c472a18d357c25a288556912dd5d2836f8d75eb9
Caio Ariel <caio.ariel@outlook.com>
2025-11-12 14:32:29 -0300

    Classe main quase finalizada

--- src/projeto_2/Main.java
+++ src/projeto_2/Main.java
@@ -1,87 +1,121 @@
 package projeto_2;
 
-/**
- * CLASSE PRINCIPAL (ORQUESTRADORA) Esta classe é o ponto de entrada do
- * programa. Suas responsabilidades são: * 1. Ler e validar os argumentos da
- * linha de comando: - <diretorio_documentos>
- * - <limiar> (grau de similaridade) - <modo> (lista, topk, busca) -
- * [argumentos_opcionais] (como o 'K' do topk ou os nomes dos arquivos da busca)
- * * 2. Orquestrar todo o fluxo de processamento: - Chamar a leitura dos
- * arquivos do diretório. - Para cada arquivo, disparar a criação do objeto
- * 'Documento' (que fará o pré-processamento). - Iniciar o loop de comparação
- * entre todos os pares de documentos. - Inserir cada 'Resultado' na 'AVLTree'.
- * * 3. Gerar a saída: - Com base no <modo>, consultar a 'AVLTree' para obter os
- * resultados corretos. - Exibir a saída formatada no terminal. - Gravar a mesma
- * saída no arquivo "resultado.txt".
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class Main {
+	
+	private static int contadorDocumentos = 0;
+	private static int contadorComparacoes = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        //Testando classe Hash ------------------------------
-        // cria a tabela com tamanho 10
-        TabelaHash tabela = new TabelaHash(10);
-
-        // inserindo palavras
-        tabela.inserir("gato", 3);
-        tabela.inserir("cachorro", 5);
-        tabela.inserir("peixe", 2);
-        tabela.inserir("gato", 7); // teste de atualização da mesma palavra
-
-        try {
-            System.out.println("Frequência de 'gato': " + tabela.busca("gato"));
-            System.out.println("Frequência de 'cachorro': " + tabela.busca("cachorro"));
-            System.out.println("Frequência de 'peixe': " + tabela.busca("peixe"));
-
-            // Testando palavra que não existe (deve lançar exceção)
-            System.out.println("Frequência de 'coelho': " + tabela.busca("coelho"));
-
-        } catch (Exception e) {
-            System.out.println("Exceção capturada: " + e.getMessage());
-        }
-        System.out.println("\n");
-        tabela.printDicio();
-        System.out.println("Quantidade de colisões: " + tabela.getColisoes());
-        //Fim do teste - apagar dps ------------------
-
-        // ========================= TESTE DO COMPARADOR (COSSSENO) =========================
-        System.out.println("\n=== Teste ComparadorCosseno ===");
-
-// Doc A
-        TabelaHash docA = new TabelaHash(17);
-        docA.inserir("dados", 3);
-        docA.inserir("ciência", 2);
-        docA.inserir("computação", 1);
-
-// Doc B
-        TabelaHash docB = new TabelaHash(17);
-        docB.inserir("dados", 1);
-        docB.inserir("ciência", 1);
-        docB.inserir("computação", 4);
-
-        ComparadorCosseno comp1 = new ComparadorCosseno(docA, docB);
-        double sim1 = comp1.similaridade();
-        System.out.println("Similaridade (docA x docB): " + sim1);
-
-// --- Caso idêntico (espera ~1.0)
-        TabelaHash docC = new TabelaHash(17);
-        docC.inserir("dados", 3);
-        docC.inserir("ciência", 2);
-        docC.inserir("computação", 1);
-
-        ComparadorCosseno comp2 = new ComparadorCosseno(docA, docC);
-        double sim2 = comp2.similaridade();
-        System.out.println("Similaridade (docA x docC - idênticos): " + sim2);
-
-// --- Caso disjunto (espera ~0.0)
-        TabelaHash docD = new TabelaHash(17);
-        docD.inserir("estruturas", 5);
-        docD.inserir("algoritmos", 2);
-
-        ComparadorCosseno comp3 = new ComparadorCosseno(docA, docD);
-        double sim3 = comp3.similaridade();
-        System.out.println("Similaridade (docA x docD - disjuntos): " + sim3);
+    	String caminho = args[0];
+    	double similaridade = Double.parseDouble(args[1]);
+    	Map<String, Documento> tabelaArquivos = Main.getTabelaArquivos(caminho);
+    	AVL arvore = Main.popularAVL(tabelaArquivos);
+    	List<Resultado> lista = new ArrayList<Resultado>();
+    	
+    	if (args.length == 3) {
+    		lista = arvore.lista(similaridade);
+    		Main.printarSaida(similaridade, lista, arvore);
+    	} else if (args.length == 4) {
+    		int K = Integer.parseInt(args[3]);
+    		lista = arvore.topK(K, similaridade);
+    		Main.printarSaida(similaridade, lista, arvore);
+    	} else if (args.length == 5) {
+    		double resultadoBusca = Main.busca(args[3], args[4], tabelaArquivos);
+    		Main.printarSaida(args[3], args[4], resultadoBusca);
+    	}
+        
+        
 
     }
+    
+    public static double busca(String arquivoA, String arquivoB, Map<String, Documento> tabelaArquivos) {
+    	if (arquivoA.equals(arquivoB)) return 1.0;
+    	
+    	Documento docA = tabelaArquivos.get(arquivoA);
+    	Documento docB = tabelaArquivos.get(arquivoB);
+    	
+    	if (docA == null || docB == null) throw new IllegalArgumentException("Arquivos invalidos.");
+    	
+    	TabelaHash tabA = docA.getTabela();
+    	TabelaHash tabB = docB.getTabela();
+    	
+    	ComparadorCosseno comparador = new ComparadorCosseno(tabA, tabB);
+    	
+    	return comparador.similaridade();
+    }
+    
+    public static Map<String, Documento> getTabelaArquivos(String caminhoPasta) throws IOException {
+    	File pasta = new File(caminhoPasta);
+        if (!pasta.isDirectory()) throw new IllegalStateException("O caminho especificado não é um diretório válido: " + pasta.getPath());
+        
+        File[] arquivosPasta = pasta.listFiles();
+        if (arquivosPasta == null) throw new IOException("Não foi possível listar os arquivos da pasta: " + pasta.getPath());
+        
+        Map<String, Documento> tabelaArquivos = new HashMap<String, Documento>();
+        for (File f: arquivosPasta) {
+        	if (f.isFile()) {
+				tabelaArquivos.put(f.getName(), new Documento(f.getPath()));
+				contadorDocumentos++;
+        	}
+        }
+        return tabelaArquivos;
+    }
+    
+    public static AVL popularAVL(Map<String, Documento> tabelaArquivos) {
+    	List<Documento> lista = new ArrayList<>(tabelaArquivos.values());
+    	AVL arvore = new AVL();
 
+    	for (int i = 0; i < lista.size(); i++) {
+    		for (int j = i + 1; j < lista.size(); j++) {
+    			
+    			Documento docA = lista.get(i);
+    			Documento docB = lista.get(j);
+    			
+    			TabelaHash tabA = docA.getTabela();
+    			TabelaHash tabB = docB.getTabela();
+    			
+    			ComparadorCosseno comparador = new ComparadorCosseno(tabA, tabB);
+    			double similaridade = comparador.similaridade();
+    			
+    			Resultado res = new Resultado(docA.getNomeArquivo(), docB.getNomeArquivo(), similaridade);
+    			arvore.inserir(res);
+    			contadorComparacoes++;
+    		}
+    	}
+    	
+    	return arvore;
+    }
+    
+    public static void printarSaida(double similaridade, List<Resultado> lista, AVL arvore) {
+    	System.out.println("=== VERIFICADOR DE SIMILARIDADE DE TEXTOS ===");
+    	System.out.println("Total de documentos processados: " + contadorDocumentos);
+    	System.out.println("Total de pares comparados: " + contadorComparacoes);
+    	System.out.println("Função hash utilizada:");
+    	System.out.println("Métrica de similaridade: Cosseno");
+    	System.out.println("Pares com similaridade >= " + similaridade + ":");
+    	System.out.println("---------------------------------");
+    	for (Resultado r : lista) {
+    		System.out.println(r.getArquivoA() + " <-> " + r.getArquivoB() + " = " + r.getSimilaridade());
+    	}
+    	System.out.println("Pares com menor similaridade:");
+    	System.out.println("---------------------------------");
+    	Resultado menor = arvore.getResultadoMenorSimilaridade();
+    	System.out.println(menor.getArquivoA() + " <-> " + menor.getArquivoB() + " = " + menor.getSimilaridade());
+    }
+    
+    public static void printarSaida(String arquivoA, String arquivoB, double similaridade) {
+    	System.out.println("=== VERIFICADOR DE SIMILARIDADE DE TEXTOS ===");
+    	System.out.println("Comparada: " + arquivoA + " <-> " + arquivoB);
+    	System.out.println("Similaridade calculada: " + similaridade);
+    	System.out.println("Métrica de similaridade: Cosseno");
+    }
 }
